941b9004c314a976bece50959e22bc0de97a0ced
krmnow <krzysiek.nowak@onet.eu>
2018-11-16 15:46:14 +0100

    Update firts_ai.py

--- firts_ai.py
+++ firts_ai.py
@@ -1,3 +1,5 @@
+
+
 import numpy as np
 
 # X = (hours sleeping, hours studying), y = score on test
@@ -32,3 +34,15 @@ S(1.79832) = .8579443067
 #weights
 self.W1 = np.random.randn(self.inputSize, self.hiddenSize) # (3x2) weight matrix from input to hidden layer
 self.W2 = np.random.randn(self.hiddenSize, self.outputSize) # (3x1) weight matrix from hidden to output layer
+
+def forward(self, X):
+#forward propagation through our network
+  self.z = np.dot(X, self.W1) # dot product of X (input) and first set of 3x2 weights
+  self.z2 = self.sigmoid(self.z) # activation function
+  self.z3 = np.dot(self.z2, self.W2) # dot product of hidden layer (z2) and second set of 3x1 weights
+  o = self.sigmoid(self.z3) # final activation function
+  return o
+
+def sigmoid(self, s):
+  # activation function
+  return 1/(1+np.exp(-s))
