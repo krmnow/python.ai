b04c1ea98ee9eddeb081cfdfc95ba66a0177fe13
krmnow <krzysiek.nowak@onet.eu>
2018-12-04 08:06:43 +0100

    Update sound.py

--- sound.py
+++ sound.py
@@ -76,3 +76,7 @@ def apply_convolution(x,kernel_size,num_channels,depth):
     weights = weight_variable([kernel_size, kernel_size, num_channels, depth])
     biases = bias_variable([depth])
     return tf.nn.relu(tf.add(conv2d(x, weights),biases))
+
+def apply_max_pool(x,kernel_size,stride_size):
+    return tf.nn.max_pool(x, ksize=[1, kernel_size, kernel_size, 1], 
+                          strides=[1, stride_size, stride_size, 1], padding='SAME')
