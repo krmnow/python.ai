d662aff130a92f2b9a0ef3af6a25247cee2bb67d
krmnow <krzysiek.nowak@onet.eu>
2018-12-04 08:05:47 +0100

    Update sound.py

--- sound.py
+++ sound.py
@@ -64,3 +64,7 @@ labels = one_hot_encode(labels)
 def weight_variable(shape):
     initial = tf.truncated_normal(shape, stddev = 0.1)
     return tf.Variable(initial)
+
+def bias_variable(shape):
+    initial = tf.constant(1.0, shape = shape)
+    return tf.Variable(initial)
