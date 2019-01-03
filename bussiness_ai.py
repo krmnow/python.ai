958d56a9ce11f73ed33c2cd2e3a258f97e426c99
krmnow <krzysiek.nowak@onet.eu>
2019-01-03 15:38:32 +0100

    define locations

--- bussiness_ai.py
+++ bussiness_ai.py
@@ -2,3 +2,16 @@ import numpy as np
 #setting the parameters gamma and alpha for the q-learning
 gamma = 0.75
 alpha = 0.9
+
+location_to_state = {'A' : 0, 
+                     'B' : 1,
+                     'C' : 2,
+                     'D' : 3,
+                     'E' : 4,
+                     'F' : 5,
+                     'G' : 6,
+                     'H' : 7,
+                     'I' : 8,
+                     'J' : 9,
+                     'K' : 10,
+                     'L' : 11}
