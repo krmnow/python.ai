56e354ed90a4cc48e4bc283b988e40b8bfece186
krmnow <krzysiek.nowak@onet.eu>
2019-01-04 12:23:15 +0100

    Update bussiness_ai.py

--- bussiness_ai.py
+++ bussiness_ai.py
@@ -25,16 +25,33 @@ R = np.array([[0,1,0,0,0,0,0,0,0,0,0,0],
               [0,0,0,0,0,0,0,0,1,0,0,0],
               [0,1,0,0,0,0,0,0,0,1,0,0],
               [0,0,1,0,0,0,1,1,0,0,0,0],
-              [],
-              [],
-              []])
+              [0,1,0,0,0,0,0,0,0,1,0,0],
+              [0,1,0,0,0,0,0,0,0,1,0,0],
+              [0,1,0,0,0,0,0,0,0,1,0,0]])
 
 Q = np.array(np.zeros([12,12]))
-
+ 
 for i in range(1000):
     current_state = np.random.randint(0,12)
     playable_actions = []
     for j in range(12):
         if R[current_state, j] > 0:
             playable_actions.append(j)
-    np.random.choice(playable_actions) 
+    next_state = np.random.choice(playable_actions)
+    TD = R[current_state, next_state] + gamma * Q[next_state, np.argmax(Q[next_state,])] - Q[current_state, next_state]
+    Q[current_state, next_state] += alpha * TD
+    
+print(Q.astype(int)) 
+
+state_to_location = {state: location for location, state in location_to_state.items()}
+
+def route(starting_location, ending_location):
+    route = [starting_location]
+    next_location = starting_location
+    while (next_location != ending_location):
+        starting_state = location_to_state[starting_location]
+        next_state = np.argmax(Q[starting_state,])
+        next_location = state_to_location[next_state]
+        route.append(next_location)
+        starting_location = next_location
+    return route
