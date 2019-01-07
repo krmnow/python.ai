3d2cb02878988174b71b7ce3cf751d2e7d635c77
krmnow <krzysiek.nowak@onet.eu>
2019-01-07 14:36:42 +0100

    def __init__

--- envoiment.py
+++ envoiment.py
@@ -1,1 +1,6 @@
 import numpy as np
+
+class Environment(object):
+    
+    def __init__(self, optimal_temperature = (18.0, 24.0), initial_month = 0, initial_number_users = 10, initial_rate_data = 60):
+        
