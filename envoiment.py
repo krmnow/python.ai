e77f73ff6817ba03f8534757378b80fe6ea48aa5
krmnow <krzysiek.nowak@onet.eu>
2019-01-07 14:52:48 +0100

    add initialize

--- envoiment.py
+++ envoiment.py
@@ -3,4 +3,20 @@ import numpy as np
 class Environment(object):
     
     def __init__(self, optimal_temperature = (18.0, 24.0), initial_month = 0, initial_number_users = 10, initial_rate_data = 60):
+        self.monthly_atmospheric_temperatures = [1.0, 5.0, 7.0, 10.0, 11.0, 20.0, 23.0, 24.0, 22.0, 10.0, 5.0, 1.0]
+        self.initial_month = initial_month
+        self.atmospheric_temperatures = self.monthly_atmospheric_temperatures[initial_month]
+        self.optimal_temperature = optimal_temperature
+        self.min_temperature = -20
+        self.max_temperature = 80
+        self.min_numer_users = 10
+        self.max_number_users = 100
+        self.max_update_users = 5
+        self.min_rate_data = 20
+        self.max_rate_data = 300
+        self.max_update_data = 10
+        self.initial_number_users = initial_number_users
+        self.current_number_users = initial_number_users
+        self.initial_rate_data = initial_rate_data
+        self.current_rate_data = initial_rate_data
         
