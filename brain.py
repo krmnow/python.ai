07e86e34853eb1f2b30a9d9ad09608fab63d8d24
krmnow <krzysiek.nowak@onet.eu>
2019-01-08 14:15:03 +0100

    create neural network

--- brain.py
+++ brain.py
@@ -6,4 +6,9 @@ from keras.optimizers import Adam
 
 class Brain(object):
     
-    def __init__(self, learning_rate = 0.001, number_actions = 5)
+    def __init__(self, learning_rate = 0.001, number_actions = 5):
+        self.learning_rate = learning_rate
+        states = Input(shape = (3,)) 
+        x = Dense(units = 64, activation = 'sigmoid')(states)
+        y = Dense(units = 32, activation = 'sigmoid')(x)
+        q_values = Dense(units = number_actions, activation = 'softmax')(y)
