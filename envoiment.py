7c47f2c09bb1d172f789941b037bddee62c7e27b
krmnow <krzysiek.nowak@onet.eu>
2019-01-08 09:29:46 +0100

    Update envoiment.py

--- envoiment.py
+++ envoiment.py
@@ -29,15 +29,50 @@ class Environment(object):
         self.train = 1
         
     def update_env(self, direction, energy_ai, month):
+        #GETTING THE REWARD
+        
+        #Comouting the energy spent by server' scooling system when there is no ai
+         
         energy_noai = 0
             if self.temperature_noqai < self.optimal_temperature[0]:
                 energy_noai = self.optimal_temperature[0] - self.temperature_noqai
                 self.temperature_noai = self.optimal_temperature[0]
             elif self.temperature_noqai > self.optimal_temperature[1]:
                 energy_noai = self.temperature_noqai - self.optimal_temperature[1]
-                
+                #cpmputing the reward
             self.reward = energy_noai - ebergu_ai
+            #scaling the reward
+            self.reward = le-3 * self.reward
             
-            sefl.reward = le-3 * self.reward
+            #GETTING THE NEXT STATE
+            
+            #Updating the atmospheric temperature
+            self.atmospheric_temperatures = monthly_atmospheric_temperatures[month]
+            #updating the numbers if users
+            self.current_number_users += np.random.randint(-self.max_update_users, self.max_update_users) 
+            if self.current_number_users > self.max_number_users:
+                self.current_number_users = self.max_number_users
+            elif self.current_number_users < self.max_number_users:
+                self.current_number_users = self.min_number_users
+            #updating the rate of data
+           self.current_number_users += np.random.randint(-self.max_update_users, self.max_update_users) 
+            if self.current_rate_data > self.max_rate_data:
+                self.current_rate_data = self.max_rate_data
+            elif self.current_rate_data < self.max_rate_data:
+                self.current_rate_data = self.min_rate_data 
+            #computing the delta of intrinsic temperature
+            past_intrinsic_temperature = self.intrinsic_temperature
+            self.intrinsic_temperature = self.atmospheric_temperatures + 1.25 * self.current_number_users + 1.25 * self.current_rate_data
+            delta_ = self.intrinsic_temperature = self.intrinsic_temperature - past_intrinsic_temperature
+            #computing the delta of temperature caused by AI
+            if direction == -1:
+                delta_detmeprature_ai = -energy_ai
+            elif direction == 1:
+                delta_detmeprature_ai = energy_ai
+            #updating the new serwr temperature caused by AI
+            self.temperature_ai += delta_intrinsic_temperature + delta_temperature_ai
+            #updating the new serwer temperature when there is no AI
+            self.temperature_noai += delta_intrinsic_temperature
+            
             
                 
