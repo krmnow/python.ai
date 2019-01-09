9e82e3540877f75edbe3aff8cff2967aa414c217
krmnow <krzysiek.nowak@onet.eu>
2019-01-09 07:40:27 +0100

    Create training.py

---
+++ training.py
@@ -0,0 +1,12 @@
+import os
+import numpy as np
+import random as rn
+import environment
+import brain
+import dqn
+
+os.environ['PYTHONHASHEED'] = '0'
+np.random.seed(12)
+rn.seed(12345)
+
+# SETTING THE PARAMETER
