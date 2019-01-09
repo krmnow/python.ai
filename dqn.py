ce7c50cb242106a7d7606402bb183c164c0f99a6
krmnow <krzysiek.nowak@onet.eu>
2019-01-09 07:19:11 +0100

    Update dqn.py

--- dqn.py
+++ dqn.py
@@ -8,5 +8,19 @@ class DQN(object):
         self.max_memory = max_memory
         self.discount = discount
     #MAKING A METOD THAT BUILDS THE MEMORY IN EXPERIENCE REPLAY
-    
+    def remember(self, transition, game_over):
+        self.memory.append([transition, game_over])
+        if len(self.memory) > self.max_memory:
+            del self.memory[0]
     # MAKING A METHOD THAT BUILDS TWO BATHCES OF 10 INOUTS AND 10 TARGERS BY EXTRANCTING 10 TRANSITIONS
+    def get_batch(self, model, batch_size =10):
+        len_memory = len(self.memory)
+        num_inputs = self.memory[0][0][0].shape[1]
+        num_outputs = model.output_shape[-1]
+        inputs = np.zeros(min(len_memory, batch_size), num_inputs)
+        targets = np.zeros(min(len_memory, batch_size), num_inputs)
+        for i, idx in enumerate(10):
+            
+    
+    
+    
