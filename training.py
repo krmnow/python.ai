087aeeaea43070ecf8baa0d268edde91863f8f70
krmnow <krzysiek.nowak@onet.eu>
2019-01-09 07:46:06 +0100

    Update training.py

--- training.py
+++ training.py
@@ -6,7 +6,14 @@ import brain
 import dqn
 
 os.environ['PYTHONHASHEED'] = '0'
-np.random.seed(12)
+np.random.seed(42)
 rn.seed(12345)
 
 # SETTING THE PARAMETER
+epsilon = .3
+number_actions = 5
+direction_boundary = (number_actions - 1) / 2
+number_epochs = 1000
+max_memory = 3000
+batch_size = 512
+temperature_step = 1.5
