6ec131e5f78a033ebc2d1fd029577d8171bb43ad
krmnow <krzysiek.nowak@onet.eu>
2019-01-10 10:20:07 +0100

    Update thompson.py

--- thompson.py
+++ thompson.py
@@ -4,4 +4,15 @@ import matplotlib.pyplot as plt
 # setting the parameter
 
 N = 10000
-D = 9
+d = 9
+
+# Creating the simulation
+
+conversion_rates = [0.05, 0.13, 0.09, 0.16, 0.11, 0.04, 0.20, 0.08, 0.01]
+X = np.array(np.zeros([N,d]))
+
+for i in range(N):
+    for j in range(d):
+        if np.random.rand() <= conversion_rates[j]:
+            X[i,j] = 1
+            
