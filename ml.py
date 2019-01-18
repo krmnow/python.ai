f387cc94600ef1081a6a5ae505bd6041ada61a4a
krmnow <krzysiek.nowak@onet.eu>
2019-01-18 11:51:27 +0100

    Update ml.py

--- ml.py
+++ ml.py
@@ -5,3 +5,7 @@ import pandas as pd
 dataset = pd.read_csv('Data.csv')
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, 3].values
+
+# taking care of missing data
+from sklearn.prepocessing import Imputer
+imputer = Imputer(missing_values = 'NaN', strategy = 'mean', axis = 0)
