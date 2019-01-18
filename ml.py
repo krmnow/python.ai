a1522ab0a544473e9467b21220756644b8316573
krmnow <krzysiek.nowak@onet.eu>
2019-01-18 12:19:16 +0100

    take care of missing data

--- ml.py
+++ ml.py
@@ -9,3 +9,5 @@ y = dataset.iloc[:, 3].values
 # taking care of missing data
 from sklearn.prepocessing import Imputer
 imputer = Imputer(missing_values = 'NaN', strategy = 'mean', axis = 0)
+imputer = imputer.fit(X[:, 1:3])
+X[:, 1:3] = imputer.transform()X[:, 1:3]
