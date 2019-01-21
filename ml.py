adda0d02aedb5eb43c4ad1bf9eaf1eec36934424
krmnow <krzysiek.nowak@onet.eu>
2019-01-21 15:32:49 +0100

    Update ml.py

--- ml.py
+++ ml.py
@@ -22,3 +22,11 @@ X = one_hot_encoder.fit_transform(X).toarray())
 
 labelencoder_y = LabelEncoder()
 y[:, 0] = labelencoder_y.fit_transform(X[:, 0])
+
+from sklearn.model_selection import train_test_split 
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
+
+# feature scaling
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
