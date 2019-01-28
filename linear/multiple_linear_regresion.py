ce254aff30a25a55c572b3f29ce48e3d3573a39e
krmnow <krzysiek.nowak@onet.eu>
2019-01-28 13:28:58 +0100

    Update multiple_linear_regresion.py

--- linear/multiple_linear_regresion.py
+++ linear/multiple_linear_regresion.py
@@ -5,6 +5,13 @@ dataset = pd.read_csv('50_startuos.csv')
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, 3].values
 
+
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder_x = LabelEncored()
+X[:, 3] = labeleencoder_x.fit_transform(X[:, 3])
+onehotencoder = OneHotEncoder(categorical_features = [3])
+X = onehotencoder.fit_transform(X).toarray()
+
 from sklearn.cross_validation import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
