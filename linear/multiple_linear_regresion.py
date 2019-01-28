fb7cf95fc3422f0ae4a0f2de6088705d1e384158
krmnow <krzysiek.nowak@onet.eu>
2019-01-28 10:26:23 +0100

    Update multiple_linear_regresion.py

--- linear/multiple_linear_regresion.py
+++ linear/multiple_linear_regresion.py
@@ -1,9 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-dataset = pd.read_csv('Data.csv')
+dataset = pd.read_csv('50_startuos.csv')
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, 3].values
 
 from sklearn.cross_validation import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+"""from.sklearn.preprocesing import StandardScaler
+sc_X = Standard_Scaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)"""
