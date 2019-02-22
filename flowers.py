dd8931260bb3d91e9c47ae3030125bfb5ba3e8e2
krmnow <krzysiek.nowak@onet.eu>
2019-02-22 10:42:50 +0100

    add y_min, x_min, y_max, x_max

--- flowers.py
+++ flowers.py
@@ -5,3 +5,6 @@ import matplotlib.pyplot as plt
 markers = ('s', '*', '^')
 colors = ('blue', 'green', 'red')
 cmap = ListedColormap(colors)
+x_min, x_max = data[:, 0].min() - 1, data[:, 0].max() + 1
+y_min, y_max = data[:, 1].min() - 1, data[:, 1].max() + 1
+resolution = 0.01
