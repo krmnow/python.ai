63813e48a44049f1f9d2521f2e7d70e26135050e
krmnow <krzysiek.nowak@onet.eu>
2019-02-22 10:48:04 +0100

    Update flowers.py

--- flowers.py
+++ flowers.py
@@ -14,3 +14,9 @@ x, y = numpy.meshgrid(numpy.arange(x_min, x_max, resolution), numpy.arange(y_min
 Z = mlp.predict(numpy.array([x.ravel(), y.ravel()]).T)
 Z = Z.reshape(x.shape)
 plt.colormersh(x, y, z, cmap=cmap)
+plt.xlim(x.min(), x.max())
+plt.ylim(y.min(), y.max())
+
+#rysowanie wykresu danyc
+
+classes = ["setosa", "versocolor", "virginica"]
