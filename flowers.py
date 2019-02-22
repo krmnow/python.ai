995a314fcbcc8f9246187617c402dc7f03862e37
krmnow <krzysiek.nowak@onet.eu>
2019-02-22 10:40:53 +0100

    Create flowers.py

---
+++ flowers.py
@@ -0,0 +1,7 @@
+import numpy
+from matplotlib.colors import ListedColormap
+import matplotlib.pyplot as plt
+
+markers = ('s', '*', '^')
+colors = ('blue', 'green', 'red')
+cmap = ListedColormap(colors)
