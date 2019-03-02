0a753d6652bedd1d30fe181869b8f08e17755f5d
krmnow <krzysiek.nowak@onet.eu>
2019-03-02 15:42:19 +0100

    add move

--- cross_round.py
+++ cross_round.py
@@ -4,3 +4,9 @@ def new_board():
   return((0,0,0),
         (0,0,0),
         (0,0,0))
+
+def aplly_move(board_state, move, side):
+  move_x, move_y = move
+  state_list = list(list(s) for s in borad_state)
+  state_list[move_x][move_y] = side
+  return tuple(tuple(s) for s in state_list)
