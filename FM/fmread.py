c67b8c0350e5c38b36ea199535e6cd629b74c289
krmnow <krzysiek.nowak@onet.eu>
2019-07-04 22:02:19 +0200

    Divide for train and test data

--- FM/fmread.py
+++ FM/fmread.py
@@ -13,3 +13,14 @@ print(dataset['IntGoals'].value_counts())
 import matplotlib.pyplot as plt
 dataset.hist(bins=50, figsize=(20,15))
 plt.show()
+
+import numpy as np
+def split_train_test(data, test_ratio):
+    shuffled_indices = np.random.permutation(len(data))
+    test_set_size = int(len(data) * test_ratio)
+    test_indices = shuffled_indices[:test_set_size]
+    train_indices = shuffled_indices[test_set_size]
+    return data.iloc[train_indices], data.iloc[test_indicies]
+
+train_set, test_set = split_train_test(dataset, 0.2)
+print("Uczące: ", len(train_set), ", testowe: ", len(test_set))
