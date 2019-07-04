cc45db39b32c31fc28c156938bbcb13c1c747c51
krmnow <krzysiek.nowak@onet.eu>
2019-07-04 21:55:50 +0200

    prepare dataset wizualization

--- FM/fmread.py
+++ FM/fmread.py
@@ -2,3 +2,14 @@ import pandas as pd
 
 def load_data(filename):
     return pd.read_csv(filename, sep = ';')
+
+lfc_data = load_data("dataset.csv")
+print(lfc_data.head(5))
+
+dataset = lfc_data
+
+print(dataset['IntGoals'].value_counts())
+
+import matplotlib.pyplot as plt
+dataset.hist(bins=50, figsize=(20,15))
+plt.show()
