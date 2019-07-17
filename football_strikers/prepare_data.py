ec8b8d8c76408c174acfa50b892393519a78a2ff
krmnow <krzysiek.nowak@onet.eu>
2019-07-17 12:03:27 +0200

    upload strikers data

--- football_strikers/prepare_data.py
+++ football_strikers/prepare_data.py
@@ -3,3 +3,13 @@ import numpy as np
 import pandas as pd
 import matplotlib as plt 
 import seaborn as sns
+
+path = 'C:\\Users\\knowak7\\Desktop\\footballAI\\football-striker-performance'
+path = path.replace("\\","/")
+os.chdir(path)
+
+def load_data(csv):
+    return pd.read_csv(csv)
+
+df = load_data('StrikerPerformance.csv')
+print(df.head(5))
