5836517e8546c8198bfc3f7e58ece913ce7157bd
krmnow <krzysiek.nowak@onet.eu>
2019-07-18 12:51:48 +0200

    try to encode position into number

--- football_strikers/prepare_data.py
+++ football_strikers/prepare_data.py
@@ -1,7 +1,7 @@
 import os
 import numpy as np
 import pandas as pd
-import matplotlib as plt 
+import matplotlib as plt
 import seaborn as sns
 
 path = 'C:\\Users\\knowak7\\Desktop\\footballAI\\football-striker-performance'
@@ -12,4 +12,17 @@ def load_data(csv):
     return pd.read_csv(csv)
 
 df = load_data('StrikerPerformance.csv')
-print(df.head(5))
+df = df.fillna(0)
+df = df.drop('nationality', axis = 1)
+
+from sklearn.preprocessing import LabelEncoder
+encoder = LabelEncoder()
+position = df['position']
+position_encoded = encoder.fit_transform(position.astype(str))
+
+from sklearn.preprocessing import StandardScaler
+scaler = StandardScaler()
+
+pos = position.values.reshape(-1,1)
+position = scaler.fit_transform(pos)
+print(position.head(5))
