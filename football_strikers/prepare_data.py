3a1bad2dc155d47bafc64d4efa9d626ebdfead00
krmnow <krzysiek.nowak@onet.eu>
2019-07-18 13:22:41 +0200

    endcode foot, league and position

--- football_strikers/prepare_data.py
+++ football_strikers/prepare_data.py
@@ -14,15 +14,41 @@ def load_data(csv):
 df = load_data('StrikerPerformance.csv')
 df = df.fillna(0)
 df = df.drop('nationality', axis = 1)
+df = df.drop('current league', axis = 1)
 
 from sklearn.preprocessing import LabelEncoder
 encoder = LabelEncoder()
+
+
+#pozycja
 position = df['position']
 position_encoded = encoder.fit_transform(position.astype(str))
 
-from sklearn.preprocessing import StandardScaler
-scaler = StandardScaler()
+#noga
+Foot = df['foot']
+foot_encoded = encoder.fit_transform(position.astype(str))
 
-pos = position.values.reshape(-1,1)
-position = scaler.fit_transform(pos)
-print(position.head(5))
+#current club
+club = df['current club']
+club_encoded = encoder.fit_transform(club.astype(str))
+
+from sklearn.preprocessing import OneHotEncoder
+encoder = OneHotEncoder()
+
+#pozycja
+position_description_1hot = encoder.fit_transform(position_encoded.reshape(-1,1))
+df['position'] = position_encoded
+
+#noga
+foot_1hot = encoder.fit_transform(foot_encoded.reshape(-1,1))
+df['foot'] = foot_encoded
+
+#current club
+club_1hot = encoder.fit_transform(club_encoded.reshape(-1,1))
+df['current club'] = club_encoded
+
+
+print(position_description_1hot.toarray())
+print(foot_1hot.toarray())
+print(club_1hot.toarray())
+
