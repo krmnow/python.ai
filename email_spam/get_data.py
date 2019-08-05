6c7106ee1c98c25e171c7663e47e45bba25c1adf
krmnow <krzysiek.nowak@onet.eu>
2019-08-05 12:37:17 +0200

    Update get_data.py

--- email_spam/get_data.py
+++ email_spam/get_data.py
@@ -19,3 +19,13 @@ def fetch_spam_data(spam_url=SPAM_URL, spam_path=SPAM_PATH):
         tar_bz2_file.close()
 
 fetch_spam_data()
+
+
+HAM_DIR = os.path.join(SPAM_PATH, "easy_ham")
+SPAM_DIR = os.path.join(SPAM_PATH, "spam")
+ham_filenames = [name for name in sorted(os.listdir(HAM_DIR)) if len(name) > 20]
+spam_filenames = [name for name in sorted(os.listdir(SPAM_DIR)) if len(name) > 20]
+
+print(len(ham_filenames))
+print(len(spam_filenames))
+
